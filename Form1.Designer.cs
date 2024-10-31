74e5c94f69761d792ad0c8eac55aa96bc0555767
eliaschen <eliaschen.dev@gmail.com>
2024-10-31 14:00:50 +0800

    summary switch code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,19 +45,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(219, 12);
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(292, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 21);
+            this.label1.Size = new System.Drawing.Size(124, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "猜拳遊戲";
             // 
             // scissors
             // 
-            this.scissors.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.scissors.Location = new System.Drawing.Point(95, 65);
+            this.scissors.Font = new System.Drawing.Font("PMingLiU", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.scissors.Location = new System.Drawing.Point(127, 87);
+            this.scissors.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.scissors.Name = "scissors";
-            this.scissors.Size = new System.Drawing.Size(87, 44);
+            this.scissors.Size = new System.Drawing.Size(116, 59);
             this.scissors.TabIndex = 1;
             this.scissors.Text = "剪刀";
             this.scissors.UseVisualStyleBackColor = true;
@@ -65,10 +67,11 @@
             // 
             // rock
             // 
-            this.rock.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.rock.Location = new System.Drawing.Point(223, 65);
+            this.rock.Font = new System.Drawing.Font("PMingLiU", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rock.Location = new System.Drawing.Point(297, 87);
+            this.rock.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rock.Name = "rock";
-            this.rock.Size = new System.Drawing.Size(87, 44);
+            this.rock.Size = new System.Drawing.Size(116, 59);
             this.rock.TabIndex = 2;
             this.rock.Text = "石頭";
             this.rock.UseVisualStyleBackColor = true;
@@ -76,10 +79,11 @@
             // 
             // paper
             // 
-            this.paper.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.paper.Location = new System.Drawing.Point(355, 65);
+            this.paper.Font = new System.Drawing.Font("PMingLiU", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.paper.Location = new System.Drawing.Point(473, 87);
+            this.paper.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.paper.Name = "paper";
-            this.paper.Size = new System.Drawing.Size(87, 44);
+            this.paper.Size = new System.Drawing.Size(116, 59);
             this.paper.TabIndex = 3;
             this.paper.Text = "布";
             this.paper.UseVisualStyleBackColor = true;
@@ -88,49 +92,53 @@
             // winLose
             // 
             this.winLose.AutoSize = true;
-            this.winLose.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.winLose.Location = new System.Drawing.Point(29, 189);
+            this.winLose.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.winLose.Location = new System.Drawing.Point(39, 252);
+            this.winLose.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.winLose.Name = "winLose";
-            this.winLose.Size = new System.Drawing.Size(26, 21);
+            this.winLose.Size = new System.Drawing.Size(0, 27);
             this.winLose.TabIndex = 4;
-            this.winLose.Text = "--";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(91, 149);
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(121, 199);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 21);
+            this.label2.Size = new System.Drawing.Size(120, 27);
             this.label2.TabIndex = 5;
             this.label2.Text = "玩家出了";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(91, 189);
+            this.label3.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(121, 252);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 21);
+            this.label3.Size = new System.Drawing.Size(120, 27);
             this.label3.TabIndex = 6;
             this.label3.Text = "電腦出了";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.Location = new System.Drawing.Point(91, 240);
+            this.label4.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(121, 320);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 21);
+            this.label4.Size = new System.Drawing.Size(120, 27);
             this.label4.TabIndex = 7;
             this.label4.Text = "勝負統計";
             // 
             // reset
             // 
-            this.reset.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.reset.Location = new System.Drawing.Point(477, 289);
+            this.reset.Font = new System.Drawing.Font("PMingLiU", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.reset.Location = new System.Drawing.Point(636, 385);
+            this.reset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.reset.Name = "reset";
-            this.reset.Size = new System.Drawing.Size(87, 44);
+            this.reset.Size = new System.Drawing.Size(116, 59);
             this.reset.TabIndex = 8;
             this.reset.Text = "重猜";
             this.reset.UseVisualStyleBackColor = true;
@@ -139,38 +147,38 @@
             // playerShow
             // 
             this.playerShow.AutoSize = true;
-            this.playerShow.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.playerShow.Location = new System.Drawing.Point(191, 149);
+            this.playerShow.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.playerShow.Location = new System.Drawing.Point(255, 199);
+            this.playerShow.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.playerShow.Name = "playerShow";
-            this.playerShow.Size = new System.Drawing.Size(31, 21);
+            this.playerShow.Size = new System.Drawing.Size(0, 27);
             this.playerShow.TabIndex = 9;
-            this.playerShow.Text = "---";
             // 
             // npcShow
             // 
             this.npcShow.AutoSize = true;
-            this.npcShow.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.npcShow.Location = new System.Drawing.Point(191, 189);
+            this.npcShow.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.npcShow.Location = new System.Drawing.Point(255, 252);
+            this.npcShow.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.npcShow.Name = "npcShow";
-            this.npcShow.Size = new System.Drawing.Size(31, 21);
+            this.npcShow.Size = new System.Drawing.Size(0, 27);
             this.npcShow.TabIndex = 10;
-            this.npcShow.Text = "---";
             // 
             // sourceCountShow
             // 
             this.sourceCountShow.AutoSize = true;
-            this.sourceCountShow.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.sourceCountShow.Location = new System.Drawing.Point(191, 240);
+            this.sourceCountShow.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.sourceCountShow.Location = new System.Drawing.Point(255, 320);
+            this.sourceCountShow.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.sourceCountShow.Name = "sourceCountShow";
-            this.sourceCountShow.Size = new System.Drawing.Size(31, 21);
+            this.sourceCountShow.Size = new System.Drawing.Size(0, 27);
             this.sourceCountShow.TabIndex = 11;
-            this.sourceCountShow.Text = "---";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 345);
+            this.ClientSize = new System.Drawing.Size(768, 460);
             this.Controls.Add(this.sourceCountShow);
             this.Controls.Add(this.npcShow);
             this.Controls.Add(this.playerShow);
@@ -183,8 +191,8 @@
             this.Controls.Add(this.rock);
             this.Controls.Add(this.scissors);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Form1.cs
+++ Form1.cs
@@ -23,20 +23,16 @@ namespace SAIHS_CS_2_3
         {
             updateGame(0);
         }
-
         private void rock_Click(object sender, EventArgs e)
         {
             updateGame(1);
         }
-
         private void paper_Click(object sender, EventArgs e)
         {
             updateGame(2);
         }
         private void updateGame(int player)
         {
-            // 0 == i win
-            // 1 == i lose 
             Random random = new Random();
             int npc = random.Next(0, 3);
             playerShow.Text = guessType[player];
@@ -47,37 +43,23 @@ namespace SAIHS_CS_2_3
             }
             else if (npc == 0)
             {
-                switch (player)
-                {
-                    case 1: gameState = 0; break;
-                    case 2: gameState = 1; break;
-                }
+                gameState = player == 1 ? 0 : 1;
             }
             else if (npc == 1)
             {
-                switch (player)
-                {
-                    case 0: gameState = 1; break;
-                    case 2: gameState = 0; break;
-                }
+                gameState = player == 0 ? 1 : 0;
             }
             else if(npc == 2)
             {
-                switch (player)
-                {
-                    case 0: gameState = 0; break;
-                    case 1: gameState = 1; break;
-                }
+                gameState = player == 0 ? 0 : 1;
             }
             winLose.Text = gameState == 2 ? "平手" : gameState == 0 ? "勝" : "輸";
             gameSource[gameState]++;
-            sourceCountShow.Text = $"你贏了{gameSource[0]}次 電腦贏了{gameSource[1]}次 平手{gameSource[2]}";
-
+            sourceCountShow.Text = $"你贏了{gameSource[0]}次 電腦贏了{gameSource[1]}次 平手{gameSource[2]}次";
         }
-
         private void reset_Click(object sender, EventArgs e)
         {
-            for(int i = 0;i<gameSource.Length;i++)
+            for(int i = 0;i < gameSource.Length;i++)
             {
                 gameSource[i] = 0;
             }
@@ -86,7 +68,6 @@ namespace SAIHS_CS_2_3
             {
                 i.Text = "";
             }
-
         }
     }
 }
